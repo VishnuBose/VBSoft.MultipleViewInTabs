0acc8696bf2bdc4b4d12075dc8b211e18465b0ad
Vishnu Bose <vishnu.bose95@gmail.com>
2021-10-30 00:30:27 +0530

    new changes

---
+++ VBSoft.MultipleViewInTabs/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace VBSoft.MultipleViewInTabs.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ VBSoft.MultipleViewInTabs/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+debugger;
+$(document).ready(function () {
+    $("#tabs").tabs();
+
+    $('[data-show-tab]').click(function (ev) {
+        debugger;
+        ev.preventDefault();
+        var url = $(this).data('show-tab');
+        var id = $(this).data('container-id');
+        $('#' + id).load(url);
+    });
+})
